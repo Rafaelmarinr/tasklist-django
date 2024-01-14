03ae82280e769c717bcbb0d7581e43fa1575f33d
Rafaelmarinr <rafaelmarinantonior@gmail.com>
2024-01-14 15:33:29 -0400

    set up admins

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Proyect, Task
+
+admin.site.register(Proyect)
+admin.site.register(Task)
 
 # Register your models here.
--- myapp/models.py
+++ myapp/models.py
@@ -5,7 +5,13 @@ from django.db import models
 class Proyect(models.Model):
     name = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.name
+
 class Task(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     proyect = models.ForeignKey(Proyect, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title + ' - ' + self.proyect.name
