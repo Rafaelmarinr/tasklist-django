00f8e8d0cab5c7c37d735c0316a0943f681c9b43
Rafaelmarinr <rafaelmarinantonior@gmail.com>
2024-01-16 07:09:55 -0400

    Templades and Jinja Loops

--- myapp/views.py
+++ myapp/views.py
@@ -5,18 +5,22 @@ from .models import Proyect, Task
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    title = 'Django Curse'
+    return render(request, 'index.html', {'title': title})
 
 def about(request):
-    return render(request, 'about.html')
+    username = 'Rafael'
+    return render(request, 'about.html', {'username': username})
 
 def hello(request, username):
     return HttpResponse('<h1>Hello %s</h1>' % username)
 
 def projects(request):
-    projects = list(Proyect.objects.values())
-    return render(request, 'projects.html')
+    # projects = list(Proyect.objects.values())
+    projects = Proyect.objects.all()
+    return render(request, 'projects.html', {'projects': projects})
 
 def tasks(request):
     #task = Task.objects.get(title=title)
-    return render(request, 'tasks.html')
+    tasks = Task.objects.all()
+    return render(request, 'tasks.html', {'tasks': tasks})
