7deba29025fc7dcef9537060bd13ce12c659304f
Rafaelmarinr <rafaelmarinantonior@gmail.com>
2024-01-18 07:03:35 -0400

    Form POST

--- myapp/forms.py
+++ myapp/forms.py
@@ -3,3 +3,6 @@ from django import forms
 class CreateNewTask(forms.Form):
     title = forms.CharField(label="Titulo", max_length=100)
     description = forms.CharField(label="Descripcion", widget=forms.Textarea())
+
+class CreateNewProject(forms.Form):
+    name = forms.CharField(label="Project name", max_length=200)
--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Proyect, Task
-from .forms import CreateNewTask
+from .forms import CreateNewTask, CreateNewProject
 # Create your views here.
 
 def index(request):
@@ -18,20 +18,32 @@ def hello(request, username):
 def projects(request):
     # projects = list(Proyect.objects.values())
     projects = Proyect.objects.all()
-    return render(request, 'projects.html', {'projects': projects})
+    return render(request, 'projects/projects.html', {'projects': projects})
 
 def tasks(request):
     #task = Task.objects.get(title=title)
     tasks = Task.objects.all()
-    return render(request, 'tasks.html', {'tasks': tasks})
+    return render(request, 'tasks/tasks.html', {'tasks': tasks})
 
 def create_task(request):
     if request.method == 'GET':
         #show interface
-        return render(request, 'create_task.html',{
+        return render(request, 'tasks/create_task.html',{
             'form': CreateNewTask()
         })
     else:
         Task.objects.create(title=request.POST['title'], description=request.POST['description'], proyect_id=2)
         return redirect('/tasks/')
         
+def create_project(request):
+    if request.method == 'GET':
+        return render(request, 'projects/create_project.html', {
+            'form': CreateNewProject()
+        })
+    else:
+        project = Proyect.objects.create(name = request.POST['name'])
+        print(project)
+        render(request, 'projects/create_project.html', {
+            'form': CreateNewProject()
+        })
+        return redirect('/projects')
